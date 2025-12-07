5c57f1d00744f827cfdcefe894f2ec5a8cd71c5c
Shubham15986 <shubham15986@gmail.com>
2025-12-07 06:07:36 +0530

    Edit Distnce

---
+++ DP/Edit_Distance.cpp
@@ -0,0 +1,108 @@
+#include<iostream>
+#include<algorithm>
+  #include <iomanip>
+  #include<climits>
+
+
+
+#include<unordered_map>
+#include<map>
+#include<set>
+#include<vector>
+#include <numeric>
+#include<cstring>
+
+using namespace std;
+
+
+#define int long long               
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(x) (int)(x).size()
+
+
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define repd(i, a, b) for (int i = (a); i >= (b); i--)
+#define each(x, a) for (auto &x : a)
+const int mod = 1e9 + 7;
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pii pair<int,int>
+#define vii vector<pii>
+#define in(v,n) rep(i,0,n) cin>>v[i];
+#define out(v,n) rep(i,0,n) cout<<v[i]<<" "; cout<<endl;
+#define pairout(v,n) rep(i,0,n) cout<<v[i].first<<" "<<cout<<v[i].second<<" "; cout<<endl;
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+// int solve(int n, int k, vector<int>& v) {
+//     if (k == 0) return 0;
+//     if (n == 0) return 1e9;
+
+//     if (dp[n][k] != -1) return dp[n][k];
+
+//     int not_pick = solve(n-1, k, v);
+
+//     int pick = 1e9;
+//     if (k - v[n-1] >= 0) {
+//         pick = 1 + solve(n, k - v[n-1], v);
+//     }
+
+//     return dp[n][k] = min(pick, not_pick);
+// }
+
+bool check(int m,int n){
+    return m>=1 && m<=n;
+}
+
+void boom(){
+   string a,b;cin>>a>>b;
+   int m= a.size();int n=b.size();
+
+   vvi dp(n+1,vi(m+1,0));
+   rep(i,0,n+1){
+     dp[i][0] = i;
+   }
+   rep(i,0,m+1){
+     dp[0][i] = i;
+   }
+   
+
+   rep(i,1,n+1){
+    rep(j,1,m+1){
+        if(b[i-1] == a[j-1]){
+            dp[i][j] = dp[i-1][j-1];
+        }
+        else{
+            dp[i][j] = 1 + min({dp[i-1][j], dp[i][j-1],dp[i-1][j-1]});
+        }
+    }
+   }
+   
+ 
+   
+  
+   cout<<dp[n][m]<<endl;
+  
+    
+}
+
+int32_t main() {
+    
+   int t; //cin>>t;
+   t=1;
+
+   while(t--){
+    boom();
+   }
+}
