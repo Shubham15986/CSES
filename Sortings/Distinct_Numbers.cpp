180603ac189b0ca6e5820b639bc412e5c8c4feef
Shubham15986 <shubham15986@gmail.com>
2025-12-10 02:37:51 +0530

    10 DEc

---
+++ Sortings/Distinct_Numbers.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<algorithm>
+  #include <iomanip>
+  #include<queue>
+
+
+
+#include<unordered_map>
+#include<map>
+#include<set>
+#include<vector>
+#include <numeric>
+#include<cstring>
+#include<climits>
+
+using namespace std;
+
+
+
+#define int long long               
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(x) (int)(x).size()
+
+
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define repd(i, a, b) for (int i = (a); i >= (b); i--)
+#define each(x, a) for (auto &x : a)
+const int mod = 1e9 + 7;
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pii pair<int,int>
+#define vii vector<pii>
+#define in(v,n) rep(i,0,n) cin>>v[i];
+#define out(v,n) rep(i,0,n) cout<<v[i]<<" "; cout<<endl;
+#define pairout(v,n) rep(i,0,n) cout<<v[i].first<<" "<<cout<<v[i].second<<" "; cout<<endl;
+const int INF = -1e9;
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+
+void boom(){
+   int n;cin>>n;
+   set<int> st;
+   rep(i,0,n){
+    int a;cin>>a;
+    st.insert(a);
+   }
+   cout<<st.size();
+    
+
+}
+int32_t main() {
+    
+   int t; //cin>>t;
+   t=1;
+
+   while(t--){
+    boom();
+   }
+}
