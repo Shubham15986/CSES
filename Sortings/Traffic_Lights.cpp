9a1a9e96581ab149fc19f4532ec7aa3c6c19617f
Shubham15986 <shubham15986@gmail.com>
2025-12-10 15:43:19 +0530

    10 Dec

---
+++ Sortings/Traffic_Lights.cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+#include<algorithm>
+  #include <iomanip>
+  #include<queue>
+
+
+
+#include<unordered_map>
+#include<map>
+#include<set>
+#include<vector>
+#include <numeric>
+#include<cstring>
+#include<climits>
+
+using namespace std;
+
+
+
+#define int long long               
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(x) (int)(x).size()
+
+
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define repd(i, a, b) for (int i = (a); i >= (b); i--)
+#define each(x, a) for (auto &x : a)
+const int mod = 1e9 + 7;
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pii pair<int,int>
+#define vii vector<pii>
+#define in(v,n) rep(i,0,n) cin>>v[i];
+#define out(v,n) rep(i,0,n) cout<<v[i]<<" "; cout<<endl;
+#define pairout(v,n) rep(i,0,n) cout<<v[i].first<<" "<<cout<<v[i].second<<" "; cout<<endl;
+const int INF = -1e9;
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+
+void boom(){
+    int x,n;cin>>x>>n;
+    vi v(n);
+    rep(i,0,n) cin>>v[i];
+   multiset<int> mst;
+   set<int> st;
+   mst.insert(x);
+   st.insert(0);
+   st.insert(x);
+   rep(i,0,n){
+       auto r = st.upper_bound(v[i]);
+       auto l = r;
+       l--;
+       mst.erase(mst.find(*r -*l));
+
+       mst.insert(v[i]-*l);
+       mst.insert(*r-v[i]);
+       st.insert(v[i]);
+       auto ans = mst.end();
+       ans--;
+       cout<<*ans<<" ";
+
+   }
+
+
+
+    
+
+   
+
+    
+
+}
+int32_t main() {
+    
+   int t; //cin>>t;
+   t=1;
+
+   while(t--){
+    boom();
+   }
+}
