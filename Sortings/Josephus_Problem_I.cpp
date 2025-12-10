cef2664b5bb7a462141ab4993b4785a12e63cccb
Shubham15986 <shubham15986@gmail.com>
2025-12-10 19:09:49 +0530

    10 Dec

---
+++ Sortings/Josephus_Problem_I.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+#include<algorithm>
+  #include <iomanip>
+  #include<queue>
+
+
+
+#include<unordered_map>
+#include<map>
+#include<set>
+#include<vector>
+#include <numeric>
+#include<cstring>
+#include<climits>
+
+using namespace std;
+
+
+
+#define int long long               
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(x) (int)(x).size()
+
+
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define repd(i, a, b) for (int i = (a); i >= (b); i--)
+#define each(x, a) for (auto &x : a)
+const int mod = 1e9 + 7;
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pii pair<int,int>
+#define vii vector<pii>
+#define in(v,n) rep(i,0,n) cin>>v[i];
+#define out(v,n) rep(i,0,n) cout<<v[i]<<" "; cout<<endl;
+#define pairout(v,n) rep(i,0,n) cout<<v[i].first<<" "<<cout<<v[i].second<<" "; cout<<endl;
+const int INF = 1e9+7;
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+
+void boom(){
+    int n;cin>>n;
+    set<int> st;
+    vi ans;
+    rep(i,0,n){
+        st.insert(i+1);
+    }
+    auto it = st.begin();
+    while(!st.empty()){
+        it++;
+        if(it == st.end())it = st.begin();
+        auto nex = it;
+        nex++; 
+        if(nex == st.end())nex = st.begin();
+        ans.pb(*it);
+        st.erase(it);
+        it = nex;
+    }
+    rep(i,0,ans.size()){
+        cout<<ans[i]<<" ";
+    }
+    
+}
+int32_t main() {
+    
+   int t; //cin>>t;
+   t=1;
+
+   while(t--){
+    boom();
+   }
+}
