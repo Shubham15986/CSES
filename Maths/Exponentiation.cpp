9a15b5e165ebf626558357fd07c5d0f0937de635
Shubham15986 <shubham15986@gmail.com>
2025-12-13 22:10:33 +0530

    13 Dec

---
+++ Maths/Exponentiation.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<algorithm>
+  #include <iomanip>
+  #include<queue>
+
+
+
+#include<unordered_map>
+#include<map>
+#include<set>
+#include<vector>
+#include <numeric>
+#include<cstring>
+#include<climits>
+
+using namespace std;            
+
+//#define int long long               
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(x) (int)(x).size()
+
+
+
+#define repd(i, a, b) for (int i = (a); i >= (b); i--)
+#define each(x, a) for (auto &x : a)
+const int mod = 1e9 + 7;
+#define int long long
+#define ll long long
+#define pll pair<ll,ll>
+#define vvi vector<vector<int>>
+#define vvl vector<vector<ll>>
+#define vl vector<ll>
+#define pii pair<int,int>
+#define vii vector<pii>
+#define in(v,n) rep(i,0,n) cin>>v[i];
+#define out(v,n) rep(i,0,n) cout<<v[i]<<" "; cout<<endl;
+#define pairout(v,n) rep(i,0,n) cout<<v[i].first<<" "<<cout<<v[i].second<<" "; cout<<endl;
+const int INF = -1e9;
+const int MOD = 1e9+7;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define vi vector<int>
+//using P as pair<pair<ll,ll>,ll>
+
+void boom(){
+    
+
+    
+}
+int32_t main() { 
+   int t; //cin>>t;
+   t=1;
+
+   while(t--){
+    boom();
+   }
+
+}
