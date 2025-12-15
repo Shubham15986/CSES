0f68d10d6cb96cea9d414b5e06b9d2ccb9892035
Shubham15986 <shubham15986@gmail.com>
2025-12-15 08:46:37 +0530

    15 Dec

---
+++ Introductory/Gray_Code.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+#include<algorithm>
+  #include <iomanip>
+  #include<queue>
+ 
+ 
+ 
+#include<unordered_map>
+#include<map>
+#include<set>
+#include<vector>
+#include <numeric>
+#include<cstring>
+#include<climits>
+ 
+using namespace std;
+ 
+ 
+ 
+#define int long long               
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(x) (int)(x).size()
+ 
+ 
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define repd(i, a, b) for (int i = (a); i >= (b); i--)
+#define each(x, a) for (auto &x : a)
+//onst int mod = 1e9 + 7;
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pii pair<int,int>
+#define vii vector<pii>
+#define in(v,n) rep(i,0,n) cin>>v[i];
+#define out(v,n) rep(i,0,n) cout<<v[i]<<" "; cout<<endl;
+#define pairout(v,n) rep(i,0,n) cout<<v[i].first<<" "<<cout<<v[i].second<<" "; cout<<endl;
+//const int INF = 1e9;
+const int mod = 1e9+7;
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+int powe(int a,int b,int mod){
+    int mul = 1;
+    while(b>0){
+        if(b&1) mul = (((mul)%mod) * (a%mod))%mod;
+        a = ((a%mod) * (a%mod))%mod;
+        b/=2;
+    }
+    return mul%mod;
+}
+ 
+void boom(){
+   int n;cin>>n;
+   vector<string> ans;
+   
+   for(int i=0;i<(1<<n);i++){
+      int r = i^(i>>1);
+      string res = "";
+
+      for(int j=0;j<n;j++){
+          res+= (r&(1<<(n-j-1))) ? "1": "0";
+      }
+      ans.pb(res);
+   }
+   rep(i,0,(1<<n)){
+    cout<<ans[i]<<endl;
+   }
+
+
+    
+   
+ 
+ 
+}
+int32_t main() {
+    
+   int t; //cin>>t;
+  t=1;
+ 
+   while(t--){
+    boom();
+   }
+}
