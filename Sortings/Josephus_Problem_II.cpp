ff35acecfd8255c7a3f9ebb2b2915b3e1f6d235e
Shubham15986 <shubham15986@gmail.com>
2025-12-16 19:01:27 +0530

    16 dec

---
+++ Sortings/Josephus_Problem_II.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+using namespace std;
+#include <set>
+#include <iterator>
+
+#if defined(__GNUG__) && !defined(__clang__)
+    #include <ext/pb_ds/assoc_container.hpp>
+    #include <ext/pb_ds/tree_policy.hpp>
+    using namespace __gnu_pbds;
+
+    template <class T>
+    using ordered_set = tree<
+        T,
+        null_type,
+        std::less<T>,
+        rb_tree_tag,
+        tree_order_statistics_node_update
+    >;
+
+#else
+    // ===== PBDS FALLBACK (VS Code / non-GNU) =====
+    template <class T>
+    class ordered_set {
+    private:
+        std::set<T> s;
+
+    public:
+        using iterator = typename std::set<T>::iterator;
+        using const_iterator = typename std::set<T>::const_iterator;
+        void insert(const T& x) {
+            s.insert(x);
+        }
+        void erase(const T& x) {
+            s.erase(x);
+        }
+        void erase(iterator it) {
+            s.erase(it);
+        }
+        iterator find(const T& x) {
+            return s.find(x);
+        }
+        int size() const {
+            return (int)s.size();
+        }
+        bool empty() const {
+            return s.empty();
+        }
+        iterator begin() {
+            return s.begin();
+        }
+
+        iterator end() {
+            return s.end();
+        }
+
+        // k-th smallest element (0-indexed)
+        iterator find_by_order(int k) {
+            auto it = s.begin();
+            advance(it, k);
+            return it;
+        }
+        // count of elements strictly less than x
+        int order_of_key(const T& x) {
+            return distance(s.begin(), s.lower_bound(x));
+        }
+    };
+#endif
+
+
+//#define int long long               
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(x) (int)(x).size()
+
+
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define repd(i, a, b) for (int i = (a); i >= (b); i--)
+#define each(x, a) for (auto &x : a)
+//onst int mod = 1e9 + 7;
+//#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define pii pair<int,int>
+#define vii vector<pii>
+#define in(v,n) rep(i,0,n) cin>>v[i];
+#define out(v,n) rep(i,0,n) cout<<v[i]<<" "; cout<<endl;
+#define pairout(v,n) rep(i,0,n) cout<<v[i].first<<" "<<cout<<v[i].second<<" "; cout<<endl;
+//const int INF = 1e9;
+const int mod = 1e9+7;
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+int powe(int a,int b,int mod){
+    int mul = 1;
+    while(b>0){
+        if(b&1) mul = (((mul)%mod) * (a%mod))%mod;
+        a = ((a%mod) * (a%mod))%mod;
+        b/=2;
+    }
+    return mul%mod;
+}
+
+void boom(){
+    int n,k;cin>>n>>k;
+    ordered_set<int> os;
+
+    rep(i,0,n){
+        os.insert(i+1);
+    }
+    vi ans;
+    int id =0;
+    while(!os.empty()){
+       id = (id+k)%os.size();
+       auto it = os.find_by_order(id);
+       ans.pb(*it);
+       os.erase(*it);
+    
+    }
+    rep(i,0,ans.size()){
+        cout<<ans[i]<<" ";
+    }
+    
+
+    
+
+
+}
+int32_t main() {
+    
+   int t; //cin>>t;
+  t=1;
+
+   while(t--){
+    boom();
+   }
+}
